ec9739a04fa6d7cd1c4a8df98dc71fa9e2771923
Alexandre Tavano <73032368+alexandretavanocardoso@users.noreply.github.com>
2021-03-25 10:27:53 -0300

    Add files via upload

---
+++ src/DevIO.Api/ViewModels/ProdutoImagemViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.Api.ViewModels
+{
+    public class ProdutoImagemViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [StringLength(200, ErrorMessage = "Máximo 200 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [StringLength(1000, ErrorMessage = "Máximo 1000 caracteres")]
+        public string Descrição { get; set; }
+
+        public IFormFile ImagemUpload { get; set; }
+
+        public string Imagem { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Valor { get; set; }
+        
+        [ScaffoldColumn(false)] // Faz com que não represente por um dado de entrada
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        [ScaffoldColumn(false)] // Faz com que não represente por um dado de entrada
+        public string NomeFornecedor { get; set; }
+
+    }
+}
