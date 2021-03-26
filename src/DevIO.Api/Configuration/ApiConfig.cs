447d10738cc56f3130e26a10a1e8394e7ce010f7
Alexandre Tavano <73032368+alexandretavanocardoso@users.noreply.github.com>
2021-03-26 16:07:18 -0300

    Add files via upload

---
+++ src/DevIO.Api/Configuration/ApiConfig.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection WebApiConfig(this IServiceCollection services)
+        {
+            services.AddControllers();
+            // Usado para retirar a forma padrao de erro da modelState
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+            // Abre pra quem quiser acessar
+            //services.AddCors(options => {  
+            //    options.AddPolicy("Development",
+            //                      builder => builder.AllowAnyOrigin()
+            //            .AllowAnyMethod()
+            //            .AllowAnyHeader()
+            //            .AllowCredentials());
+            //});
+
+            return services;
+        }
+
+        public static IApplicationBuilder WebApiConfig(this IApplicationBuilder app)
+        {
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            //app.UseCors("Development");
+
+            return app;
+        }
+
+    }
+}
