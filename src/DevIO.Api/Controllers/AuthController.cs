d5582d1dcaa308e4471089d168f4f5fb40539717
Alexandre Tavano <73032368+alexandretavanocardoso@users.noreply.github.com>
2021-03-29 15:22:27 -0300

    Add files via upload

---
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿
+
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : MainController
+    {
+        public AuthController(INotificador notificador) : base(notificador)
+        {
+
+        }
+    
+        public async Task<ActionResult> Registrar(RegisterUserViewModel registerUserViewModel)
+        {
+            return CustomResponse(registerUserViewModel);
+        }
+    }
+}
