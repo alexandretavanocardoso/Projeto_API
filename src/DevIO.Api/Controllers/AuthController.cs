048a0923d4603b43b4e28075b17513523092cbc2
Alexandre Tavano <73032368+alexandretavanocardoso@users.noreply.github.com>
2021-03-29 16:52:55 -0300

    Update AuthController.cs

--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -1,7 +1,8 @@
-﻿
+
 
 using DevIO.Api.ViewModels;
 using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -10,14 +11,70 @@ namespace DevIO.Api.Controllers
     [Route("api/[controller]")]
     public class AuthController : MainController
     {
-        public AuthController(INotificador notificador) : base(notificador)
-        {
+        private readonly SignInManager<IdentityUser> _signInManager; // Autenticacao usuario
+        private readonly UserManager<IdentityUser> _userManager; // cria usuario
 
-        }
-    
-        public async Task<ActionResult> Registrar(RegisterUserViewModel registerUserViewModel)
+        public AuthController(INotificador notificador,
+                              SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager) : base(notificador)
         {
-            return CustomResponse(registerUserViewModel);
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("novaConta")]
+        public async Task<ActionResult> Registrar(RegisterUserViewModel registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            // Criando usuario
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            // Gera usuario
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            if (result.Succeeded)
+            {
+                // ja faz o login do usuario caso o result for sucesso
+                await _signInManager.SignInAsync(user, false); // SignInAsync(usuario, Se é persistente)
+                return CustomResponse(registerUser);
+            }
+
+            foreach (var error in result.Errors)
+            {
+                NotificarErro(error.Description);
+            }
+
+
+            return CustomResponse(registerUser);
+        }
+
+        [HttpPost("entrar")]
+        public async Task<ActionResult> Login(LoginUserViewModel loginUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                return CustomResponse(loginUser);
+            }
+
+            // se tiver bloqueado
+            if (result.IsLockedOut)
+            {
+                NotificarErro("usuario Bloqueado");
+                return CustomResponse(loginUser);
+            }
+
+            // Caso errar os campos
+            NotificarErro("usuario e senha incorreto");
+            return CustomResponse(loginUser);
         }
     }
 }
