d19236ecf1769b15a71c572ef7f8e7a290817085
Alexandre Tavano <73032368+alexandretavanocardoso@users.noreply.github.com>
2021-03-29 11:30:45 -0300

    Add files via upload

---
+++ src/DevIO.Api/Configuration/IdentityConfig.cs
@@ -0,0 +1,30 @@
+﻿using DevIO.Api.Data;
+using DevIO.Data.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Conexão com o banco
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentityCore<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+                
+            return services;
+        }
+    }
+}
