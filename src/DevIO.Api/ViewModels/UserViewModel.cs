c3b0ddcf3b6ae4023a084c44ae87207ebe85e10b
Alexandre Tavano <73032368+alexandretavanocardoso@users.noreply.github.com>
2021-03-29 15:22:09 -0300

    Add files via upload

---
+++ src/DevIO.Api/ViewModels/UserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        [EmailAddress(ErrorMessage = "E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "senhas não conferem")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginUserViewModel
+    {
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        [EmailAddress(ErrorMessage = "E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public string Password { get; set; }
+    }
+}
