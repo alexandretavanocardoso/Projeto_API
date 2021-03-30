2b7143031616b7af305a09c498990de4386443ed
Alexandre Tavano <73032368+alexandretavanocardoso@users.noreply.github.com>
2021-03-30 10:26:01 -0300

    Update IdentityConfig.cs

--- src/DevIO.Api/Configuration/IdentityConfig.cs
+++ src/DevIO.Api/Configuration/IdentityConfig.cs
@@ -26,6 +26,33 @@ namespace DevIO.Api.Configuration
                 .AddErrorDescriber<IdentityMensagemPortugues>()
                 .AddDefaultTokenProviders();
                 
+             // JWT - Json Web Token
+
+            // Configurando
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            // Obtendo os dados
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret); // Definindo uma chave
+
+            services.AddAuthentication(x => {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // gera token
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; // quando gera token ver a validacao
+            }).AddJwtBearer(x => {
+                x.RequireHttpsMetadata = false; // se for trabalhar apenas com https - deixar true
+                x.SaveToken = true; // Se token deve ser guardado após autenticacao com sucesso
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true, // Valida se o emitor tem que ser o mesmo do token com base na chave
+                    IssuerSigningKey = new SymmetricSecurityKey(key), // Configurando chave
+                    ValidateIssuer = true, // Valida o emitor
+                    ValidateAudience = true, // Valida onde o token é valido
+                    ValidAudience = appSettings.ValidoEm, // configurando Audience
+                    ValidIssuer = appSettings.Emissor // Configurando emissor
+                };
+            });
+            
             return services;
         }
     }
