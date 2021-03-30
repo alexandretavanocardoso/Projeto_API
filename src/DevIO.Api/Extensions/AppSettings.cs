3d5fd5b39a25ba7485238803bbcc4c0a9234c5d9
Alexandre Tavano <73032368+alexandretavanocardoso@users.noreply.github.com>
2021-03-30 10:26:33 -0300

    Add files via upload

---
+++ src/DevIO.Api/Extensions/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Extensions
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; } // chave de criptografia
+        public int ExpiracaoHoras { get; set; } // horas da validade
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; } // urls q token é valido
+    }
+}
